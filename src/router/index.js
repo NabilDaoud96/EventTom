83c54d81e154792a5cdfdd54067b5ff2bf90a9ba
LasseRhys <selenfresser@gmx.de>
2024-12-27 23:07:24 +0100

    IndexBuyTicketsundVoucher

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,27 @@ import { createRouter, createWebHistory } from "vue-router";
 import UserLogin from "../views/UserLogin.vue"; // Import der Login-Seite
 
 const routes = [
-  {
-    path: "/login",
-    name: "UserLogin",
-    component: UserLogin, // Diese Komponente wird geladen, wenn die URL "/login" ist
-  },
-  // Hier kannst du später weitere Seiten wie Dashboard hinzufügen
+    {
+        path: "/login",
+        name: "UserLogin",
+        component: UserLogin,
+    },
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        component: Dashboard, // Dashboard Route
+    },
+    {
+        path: "/redeem-voucher",
+        name: "RedeemVoucher",
+        component: RedeemVoucher, // Gutschein-Seite
+    },
+    {
+        path: "/buy-tickets",
+        name: "BuyTickets",
+        component: BuyTickets, // Ticketkauf-Seite
+    },
+    // Weitere Routen...
 ];
 
 const router = createRouter({
